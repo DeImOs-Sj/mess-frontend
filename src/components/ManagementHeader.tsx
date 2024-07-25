c2a80841d2b983d20e8705b4468d2b73ed3ae2fd
Shlok Main <shlokjagtap123@gmail.com>
2024-07-25 10:37:54 +0530

    manager header state management

--- src/components/ManagementHeader.tsx
+++ src/components/ManagementHeader.tsx
@@ -13,8 +13,8 @@ import {
 } from "./ui/dropdown-menu";
 
 export default function ManagementHeader() {
-  let [user] = useAtom(userDetailsAtom);
-  let [, setIsloggedin] = useAtom(loginAtom);
+  const [user] = useAtom(userDetailsAtom);
+  const [, setIsloggedin] = useAtom(loginAtom);
 
   const logout = () => {
     console.log("hello");
@@ -25,10 +25,7 @@ export default function ManagementHeader() {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={SinhgadLogo} className="h-8 mt-2" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Mess Feedback System
@@ -84,7 +81,7 @@ export default function ManagementHeader() {
             </li>
 
             <li>
-              {user?.role !== "STUDENT" && (
+              {user?.role !== "MANAGER" && (
                 <Link
                   to="/create-manager"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
