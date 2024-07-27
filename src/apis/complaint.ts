6873fc2ec3e28f46d0d2aba87af7467d0a894b0b
Shlok Main <shlokjagtap123@gmail.com>
2024-07-27 14:22:43 +0530

    student header made responsive

--- src/apis/complaint.ts
+++ src/apis/complaint.ts
@@ -1,156 +1,132 @@
-import { Complaint, DashboardData, DetailMessInfo, MessInfo } from "../interfaces"
+import {
+  Complaint,
+  DashboardData,
+  DetailMessInfo,
+  MessInfo,
+} from "../interfaces";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+export async function createComplaint(
+  token: string,
+  complaint: Complaint
+): Promise<boolean> {
+  try {
+    const resp = await fetch(BACKEND_URL + "/complaint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        email: complaint.email,
+        campus: complaint.campus,
+        mess: complaint.mess,
+        date_of_happening: complaint.date_of_happening,
+        student_name: complaint.student_name,
+        student_phno: complaint.student_phno,
+        college_name: complaint.college_name,
+        is_clean: JSON.parse(complaint.is_clean),
+        is_pest_controlled: JSON.parse(complaint.is_pest_controlled),
+        food_handler_protocols: JSON.parse(complaint.food_handler_protocols),
+        complaint_desc: complaint.complaint_desc,
+        suggestion_improvement: complaint.suggestion_improvement,
+        complaint_category: complaint.complaint_category,
+        meal_time: complaint.meal_time,
+        image_photos: [],
+      }),
+    });
 
-export async function createComplaint(token: string, complaint: Complaint): Promise<boolean> {
-
-    try {
-
-        const resp = await fetch(
-            BACKEND_URL + "/complaint",
-            {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify({
-                    email: complaint.email,
-                    campus: complaint.campus,
-                    mess: complaint.mess,
-                    date_of_happening: complaint.date_of_happening,
-                    student_name: complaint.student_name,
-                    student_phno: complaint.student_phno,
-                    college_name: complaint.college_name,
-                    is_clean: JSON.parse(complaint.is_clean),
-                    is_pest_controlled: JSON.parse(complaint.is_pest_controlled),
-                    food_handler_protocols: JSON.parse(complaint.food_handler_protocols),
-                    complaint_desc: complaint.complaint_desc,
-                    suggestion_improvement: complaint.suggestion_improvement,
-                    complaint_category: complaint.complaint_category,
-                    meal_time: complaint.meal_time,
-                    image_photos: [],
-                }),
-            }
-        )
-
-        if (resp.status !== 201) {
-            return false
-        }
-
-        return true
-
-
-    } catch (err: any) {
-        console.log(err)
-        return false
+    if (resp.status !== 201) {
+      return false;
     }
 
+    return true;
+  } catch (err: any) {
+    console.log(err);
+    return false;
+  }
 }
 
+export async function getDashboard(
+  token: string
+): Promise<DashboardData | null> {
+  try {
+    const resp = await fetch(BACKEND_URL + "/complaint/dashboard", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-
-export async function getDashboard(token: string): Promise<DashboardData | null> {
-
-    try {
-
-        const resp = await fetch(
-            BACKEND_URL + "/complaint/dashboard",
-            {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            }
-        )
-    
-        if (resp.status !== 200) {
-            return null;
-        }
-        
-        const jsn: DashboardData = await resp.json();
-
-
-        return jsn;
-
-
-    } catch (err: any) {
-        console.log(err)
-        return null
+    if (resp.status !== 200) {
+      return null;
     }
 
+    const jsn: DashboardData = await resp.json();
+
+    return jsn;
+  } catch (err: any) {
+    console.log(err);
+    return null;
+  }
 }
 
+export async function getComplaints(
+  token: string,
+  status: number
+): Promise<MessInfo[]> {
+  try {
+    const resp = await fetch(
+      BACKEND_URL +
+        `/complaint?page=1&limit=100&sortBy=createdAt&status=${status}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-
-export async function getComplaints(token: string, status: number): Promise<MessInfo[]> {
-
-    try {
-
-        const resp = await fetch(
-            BACKEND_URL + `/complaint?page=1&limit=100&sortBy=createdAt&status=${status}`,
-            {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-            }
-        )
-    
-        if (resp.status !== 200) {
-            return [];
-        }
-        
-        const jsn: MessInfo[] = await resp.json();
-        console.log(jsn)
-
-
-        return jsn;
-
-
-    } catch (err: any) {
-        console.log(err)
-        return []
+    if (resp.status !== 200) {
+      return [];
     }
 
+    const jsn: MessInfo[] = await resp.json();
+    console.log(jsn);
+
+    return jsn;
+  } catch (err: any) {
+    console.log(err);
+    return [];
+  }
 }
 
+export async function getComplaint(
+  token: string,
+  complaintId: number
+): Promise<DetailMessInfo | null> {
+  try {
+    const resp = await fetch(BACKEND_URL + `/complaint/${complaintId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-
-
-
-export async function getComplaint(token: string, complaintId: number): Promise<DetailMessInfo | null> {
-
-    try {
-
-        const resp = await fetch(
-            BACKEND_URL + `/complaint/${complaintId}`,
-            {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-            }
-        )
-    
-        if (resp.status !== 200) {
-            return null;
-        }
-        
-        const jsn: DetailMessInfo = await resp.json();
-        console.log(jsn)
-
-
-        return jsn;
-
-
-    } catch (err: any) {
-        console.log(err)
-        return null
+    if (resp.status !== 200) {
+      return null;
     }
 
+    const jsn: DetailMessInfo = await resp.json();
+    console.log(jsn);
+
+    return jsn;
+  } catch (err: any) {
+    console.log(err);
+    return null;
+  }
 }
--- src/components/StudentHeader.tsx
+++ src/components/StudentHeader.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useAtom } from "jotai";
 import SinhgadLogo from "../assets/header/sinhgad.png";
 import { loginAtom, userDetailsAtom } from "../atoms/autAtom";
@@ -13,6 +14,7 @@ import {
 } from "./ui/dropdown-menu";
 
 export default function StudentHeader() {
+  const [isOpen, setIsOpen] = React.useState(false);
   let [user] = useAtom(userDetailsAtom);
   let [, setIsloggedin] = useAtom(loginAtom);
 
@@ -20,6 +22,10 @@ export default function StudentHeader() {
     console.log("hello");
     localStorage.removeItem("access");
     setIsloggedin(false);
+  };
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -53,8 +59,32 @@ export default function StudentHeader() {
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
+        <button
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-sticky"
+          aria-expanded="false"
+          onClick={toggle}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5h14a1 1 0 110 2H3a1 1 0 110-2zm0 4h14a1 1 0 110 2H3a1 1 0 110-2zm0 4h14a1 1 0 110 2H3a1 1 0 110-2z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className={`${
+            isOpen ? "block" : "hidden"
+          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
           id="navbar-sticky"
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
